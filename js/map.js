c76c5ae52963f63cab3db3416435ca90bc0fd928
lindsaycarbonell <lindsaycarbonell@gmail.com>
2017-02-11 13:59:41 -0500

    removed show all button and showed n.c. on load

--- js/map.js
+++ js/map.js
@@ -2,6 +2,7 @@ console.log("start");
 
 var mymap;
 var mapData;
+var markers = [];
 
 window.onload = function() { init() };
 
@@ -36,52 +37,46 @@ window.onload = function() { init() };
       //THE ZOOM IS SET UP HERE DUMMY
 
       var places = mapData;
-      var markers = [];
+
 
       // L.tileLayer('https://api.mapbox.com/styles/v1/lindsaycarbonell/citeusxid006x2ip17zp0ed55/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibGluZHNheWNhcmJvbmVsbCIsImEiOiJjaXRlajNhd2cwNjBkMzJvOW04OWQ0dm5xIn0.GGAg70cv_JpPUXxFvkdY-w').addTo(mymap);
       L.tileLayer('https://api.mapbox.com/styles/v1/lindsaycarbonell/ciyuctvpo002k2rmx5xriumbr/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibGluZHNheWNhcmJvbmVsbCIsImEiOiJjaXRlajNhd2cwNjBkMzJvOW04OWQ0dm5xIn0.GGAg70cv_JpPUXxFvkdY-w').addTo(mymap);
 
       mymap.scrollWheelZoom.disable();
 
+      $('#lister').append('<div><h2>' + places[0].name + '</h2><h3 style="display:block;">President: ' + places[0].presidents + '</h3><img class="list-img" src="assets/' + places[0].photo + '"/><a class="list-link" href="' + places[0].website + '">Visit Website</a><a class="list-link" href="' + places[0].facebook + '">Visit Facebook</a></div>');
 
-      for (var x in places){
 
-        var thisIcon = L.icon({
-          iconUrl: 'assets/map-scott.png',
-          iconSize:     [38, 38], // size of the icon
-          iconAnchor:   [22, 38], // point of the icon which will correspond to marker's location
-          popupAnchor:  [-3, -45] // point from which the popup should open relative to the iconAnchor
 
-        });
+      for (var x = 1; x < places.length; x++){
+        console.log(places[x].name + " " + x);
 
-        var thisMarker = L.marker([places[x].lat, places[x].long], {icon: thisIcon})
-            .on('click', onClickMarker.bind(places[x]))
-            .bindPopup("<h2>" + places[x].name + "</h2>" )
-            .openPopup();
 
-        // console.log(thisMarker);
-        markers.push(thisMarker);
-        // console.log("markers: " + markers);
-        markers[x].addTo(mymap);
-      }
+          console.log("not nc");
+          var thisIcon = L.icon({
+            iconUrl: 'assets/map-scott.png',
+            iconSize:     [38, 38], // size of the icon
+            iconAnchor:   [22, 38], // point of the icon which will correspond to marker's location
+            popupAnchor:  [-3, -45] // point from which the popup should open relative to the iconAnchor
+
+          });
+
+          var thisMarker = L.marker([places[x].lat, places[x].long], {icon: thisIcon})
+              .on('click', onClickMarker.bind(places[x]))
+              .bindPopup("<h2>" + places[x].name + "</h2>" )
+              .openPopup();
+
+
+            console.log('markers is empty');
+            markers.push(thisMarker);
+
+            markers[x-1].addTo(mymap);
+            console.log(markers[x]);
+          }
+        }
+
 
       function onClickMarker(){
-          $('#view-all').show();
           $('#lister').empty();
           $('#lister').append('<div><h2>' + this.name + '</h2><h3 style="display:block;">President: ' + this.presidents + '</h3><img class="list-img" src="assets/' + this.photo + '"/><a class="list-link" href="' + this.website + '">Visit Website</a><a class="list-link" href="' + this.facebook + '">Visit Facebook</a></div>');
       }
-
-
-
-    }
-
-    $('#view-all').on('click', function(){
-      $('#lister').empty();
-      $('#view-all').hide();
-      console.log(mapData);
-      for (var x in mapData){
-        $('#lister').append('<div><h2>' + mapData[x].name + '</h2><h3 style="display:block;">President: ' + mapData[x].presidents + '</h3><img class="list-img" src="assets/' + mapData[x].photo + '"/><a class="list-link" href="' + mapData[x].website + '">Visit Website</a><a class="list-link" href="' + mapData[x].facebook + '">Visit Facebook</a></div>');
-      }
-
-
-    });
